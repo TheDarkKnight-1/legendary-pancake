48dbfb321b60fc2b5b41b28d62849bf657a7c6e3
TheDarkKnight-1 <chandanapmishra2017@gmail.com>
2021-10-16 06:17:07 +0000

    update

--- src/components/TextForm.jsx
+++ src/components/TextForm.jsx
@@ -1,12 +1,19 @@
 import React, { useState } from 'react'
 import { Form, Button } from 'react-bootstrap'
 export default function TextForm(props) {
-    const [text, setText] = useState("Enter your text here")
+    const [text, setText] = useState("")
     const toUpper=()=>{
         console.log("toUpper")
         let va=text
         va=va.toUpperCase()
         setText(va)
+    }
+    const intoTitleCase = () => {
+        let newText = text.split(" ").map((currentValue) => {
+            let newText = currentValue[0].toUpperCase() + currentValue.slice(1);
+            return newText;
+        });
+        setText(newText.join(" "));
     }
     return (
         <div>
@@ -16,9 +23,26 @@ export default function TextForm(props) {
                     <Form.Control as="textarea" value={text} onChange={(event)=>{setText(event.target.value)}} rows={10} />
                 </Form.Group>
             </Form>
-            <Button variant="primary" type="submit" onClick={toUpper}>
-                Submit
+            <Button className="mx-1" variant="primary" type="submit" onClick={toUpper}>
+                Upper
             </Button>
+            <Button className="mx-1" variant="primary" type="submit" onClick={()=>{setText(text.toLowerCase())}}>
+                Lower
+            </Button>
+            <Button className="mx-1" variant="primary" type="submit" onClick={intoTitleCase}>
+                Title Case
+            </Button>
+            <Button className="mx-1" variant="primary" type="submit" onClick={()=>{setText("")}}>
+                Clear
+            </Button>
+            <div className="container my-4">
+                <h5>Text Summary</h5>
+                <p>{text.split(" ").length-1} words and {text.length} characters</p>
+                <p>Average Reading Time: {0.008*(text.split(" ").length-1)} mins</p>
+                <h5>Preview</h5>
+                <p>{text}</p>
+            </div>
+            
         </div>
     )
 }
