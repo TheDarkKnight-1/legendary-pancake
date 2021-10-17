bb82150d2db507a3a51db916a4b37a710a618a92
TheDarkKnight-1 <chandanapmishra2017@gmail.com>
2021-10-17 07:25:08 +0000

    react router added

--- src/App.js
+++ src/App.js
@@ -5,19 +5,31 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar'
 import TextForm from './components/TextForm';
 import About from './components/About';
-import SentimentAnalyzer from './components/SentimentAnalyzer';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <>
-      <NavBar title="Sentiment Analyzer" />
-      <div className="container my-3"> 
-        <TextForm heading="Enter the text to analyze" />
-      </div>
-      {/* <div className="container my-3"> 
-        <SentimentAnalyzer/>
-      </div> */}
-      {/* <About/> */}
+      <Router>
+        <NavBar title="Sentiment Analyzer" />
+
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/">
+            <div className="container my-3">
+              <TextForm heading="Enter the text to analyze" />
+            </div>
+          </Route>
+        </Switch>
+      </Router>
+
     </>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,6 +3,8 @@ import { Navbar, NavDropdown, Nav, Container, Form, FormControl, Button } from '
 import 'bootstrap/dist/css/bootstrap.min.css';
 import PropTypes from 'prop-types'
 import { propTypes } from 'react-bootstrap/esm/Image';
+import {Link} from "react-router-dom";
+import {LinkContainer} from 'react-router-bootstrap'
 
 function NavBar(props) {
     return (
@@ -13,15 +15,12 @@ function NavBar(props) {
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
-                            <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link href="#link">Link</Nav.Link>
-                            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                            </NavDropdown>
+                        <LinkContainer to="/">
+                            <Nav.Link>Home</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="About">
+                            <Nav.Link>About</Nav.Link>
+                        </LinkContainer>
                         </Nav>
                     </Navbar.Collapse>
                     <Form className="d-flex">
