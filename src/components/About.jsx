5f73090bff76a5c4a6e82954827440d5a0b25125
TheDarkKnight-1 <chandanapmishra2017@gmail.com>
2021-10-18 05:45:44 +0000

    deployment bug fixed in package.json file

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -26,27 +26,22 @@ export default function About() {
             <h1>About</h1>
             <Accordion defaultActiveKey="0" >
                 <Accordion.Item eventKey="0" style={myStyle}>
-                    <Accordion.Header>Accordion Item #1</Accordion.Header>
+                    <Accordion.Header>Developer</Accordion.Header>
                     <Accordion.Body>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                        est laborum.
+                    <p>👋 Hi I'm Chandan Mishra</p>
+                    <p>I am a Cloud and ML enthusiast. My expertise includes project design and management, data analysis, development and implementation of research tools.
+                    I enjoy generating new ideas and devising feasible solutions to broadly relevant problems.</p>
                     </Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item eventKey="1" style={myStyle}>
-                    <Accordion.Header>Accordion Item #2</Accordion.Header>
+                    <Accordion.Header>Tech Stack</Accordion.Header>
                     <Accordion.Body>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                        est laborum.
+                        <ul>
+                            <li>ReactJS</li>
+                            <li>HTML</li>
+                            <li>CSS</li>
+                            <li>Machine Learning (Javascript)</li>
+                        </ul>
                     </Accordion.Body>
                 </Accordion.Item>
             </Accordion>
